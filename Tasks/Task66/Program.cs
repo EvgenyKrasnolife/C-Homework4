9e17b9b0217135e0b6141e9d1801abbf1cac4be4
Евгений <redcheeken@yandex.ru>
2022-09-19 14:16:17 +0300

    Task66 Seminar 9

---
+++ Tasks/Task66/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+int InputNum(string a)
+{
+	Console.WriteLine(a);
+	return Convert.ToInt32(Console.ReadLine());
+}
+
+int SummNaturalNumbers(int start, int finish)
+{
+	if (start == finish) return finish;
+	int summ = start;
+	start++;
+	return summ + SummNaturalNumbers(start, finish);
+}
+
+
+int numStart = InputNum("Введите значение M: ");
+int numFinish = InputNum("Введите значение N: ");
+Console.WriteLine("Сумма натуральных элементов в промежутке от M до N: ");
+Console.WriteLine(SummNaturalNumbers(numStart, numFinish));
